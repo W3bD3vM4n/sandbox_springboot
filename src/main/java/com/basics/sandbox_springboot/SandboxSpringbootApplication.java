f209fe8d22d4f3cc21366952e8070f4ae1489edc
Hassan Rahman <hr-awesome@hotmail.com>
2024-12-11 16:48:39 -0500

    First commit

---
+++ src/main/java/com/basics/sandbox_springboot/SandboxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.basics.sandbox_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxSpringbootApplication.class, args);
+	}
+
+}
