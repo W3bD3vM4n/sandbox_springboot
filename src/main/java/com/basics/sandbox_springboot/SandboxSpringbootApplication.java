ca50c61f00615e16e95472041423af988bb3ad81
Hassan Rahman <hr-awesome@hotmail.com>
2024-12-16 17:54:29 -0500

    Fix to default project

--- src/main/java/com/basics/sandbox_springboot/SandboxSpringbootApplication.java
+++ src/main/java/com/basics/sandbox_springboot/SandboxSpringbootApplication.java
@@ -1,19 +1,13 @@
 package com.basics.sandbox_springboot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SandboxSpringbootApplication {
 
-	private static Logger logger = LoggerFactory.getLogger(SandboxSpringbootApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(SandboxSpringbootApplication.class, args);
-
-		logger.debug("Mi mensaje debug");
 	}
 
 }
